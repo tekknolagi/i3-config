040bdd7450e447d416e27a5a2de90cd4ff974c80
Maxwell Bernstein <tekknolagi@gmail.com>
2017-07-13 18:42:49 +0200

    Updates

--- lockscreen.sh
+++ lockscreen.sh
@@ -4,5 +4,8 @@
 #convert /tmp/screen_locked.png -scale 5% -scale 2000% /tmp/screen_locked2.png
 #convert /tmp/screen_locked.png -blur 0x16 /tmp/screen_locked2.png
 #i3lock -i /tmp/screen_locked2.png -d -f
-i3lock --no-unlock-indicator --dpms --show-failed-attempts \
-  --image=/home/max/Pictures/Wallpapers/yosemite1-blur-lock.png
+
+# file must be 1920x180 -- use convert -resize "1920x1080"
+WALLPAPER_IMG="japan_1980s.png"
+i3lock --dpms --show-failed-attempts --tiling \
+  --image="/home/max/Pictures/Wallpapers/${WALLPAPER_IMG}"
